68fa5ddb32a693d545342aff400584a7dc5c3546
Florian Margaine <florian@margaine.com>
2012-08-31 02:34:36 +0200

    npm modules

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ ln -s .dotfiles/jshintrc .jshintrc
 # Dependencies
 sudo apt-get install curl tmux mercurial python-dev ruby-dev linux-headers-$(uname -r) g++ make
 
-# Download and install vim
+# Vim
 hg clone https://vim.googlecode.com/hg/ vim
 cd vim/
 ./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp --enable-cscope
@@ -32,7 +32,7 @@ sudo make install
 cd ~
 rm -rf vim/
 
-# Download and install node.js
+# Node.js
 git clone https://github.com/joyent/node.git
 cd node/
 git checkout origin/v0.8.8-release
@@ -41,4 +41,7 @@ make
 sudo make install
 cd ~
 rm -rf node/
+
+# Npm modules
+sudo npm install -g jshint browserify
 
